01d41e15398f71e1c5f259650d1f2f87ba8e3461
AnastasiyaKl <anastasiakovalenko886@gmail.com>
2018-12-18 18:37:05 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<img alt="Vue logo" src="./assets/4.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<newElement v-bind:va="message" msg="Just test header" />-->
+    <ProductApp/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+/*import newElement from './components/newElement'*/
+import ProductApp from './components/ProductApp.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    ProductApp,
+    HelloWorld,
+  },
+  /*data: () => {
+    return {
+      message: "new header"
+    }
+  }*/
+
+
 }
 </script>
 
